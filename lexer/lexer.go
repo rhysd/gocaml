801cc53ac12f3117b60a3546669afa3bbef30bc4
rhysd <lin90162@yahoo.co.jp>
2017-01-16 01:42:45 +0900

    first MinCaml lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,152 +1,352 @@
 package lexer
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"github.com/rhysd/mincaml-parser/token"
 	"io"
-	"text/scanner"
+	"unicode"
+	"unicode/utf8"
 )
 
 type stateFn func(*Lexer) stateFn
 
-type LexError struct {
-	ErrorCount int
-	Filename   string
-}
-
-func (e *LexError) Error() string {
-	return fmt.Sprintf("%d errors found on lexing '%s'", e.ErrorCount, e.Filename)
-}
+const eof = -1
 
 type Lexer struct {
-	state  stateFn
-	scan   scanner.Scanner
-	pos    scanner.Position
-	tokens chan token.Token
-	// errors chan error
-}
-
-func LexAll(file string, src io.Reader) ([]token.Token, error) {
-	c := make(chan token.Token)
-	ts := []token.Token{}
-	l := NewLexer(file, src, c)
-	go l.Lex()
-	for {
-		select {
-		case t := <-c:
-			switch t.Kind {
-			case token.EOF:
-				return ts, nil
-			case token.ILLEGAL:
-				return nil, fmt.Errorf("Error at %s", t.String())
-			default:
-				ts = append(ts, t)
-			}
-		}
-	}
+	state   stateFn
+	start   token.Position
+	current token.Position
+	buf     bytes.Buffer
+	src     *bufio.Reader
+	tokens  chan token.Token
+	top     rune
+	eof     bool
+	file    string
 }
 
 func NewLexer(file string, src io.Reader, tokens chan token.Token) *Lexer {
-	var s scanner.Scanner
-	s.Filename = file
-	s.Init(src)
-	s.Mode = scanner.ScanIdents | scanner.ScanInts | scanner.ScanFloats
+	start := token.Position{1, 1, 0}
 	return &Lexer{
-		scan:   s,
-		tokens: tokens,
+		state:   lex,
+		start:   start,
+		current: start,
+		src:     bufio.NewReader(src),
+		tokens:  tokens,
+		file:    file,
 	}
 }
 
-func (l *Lexer) Lex() error {
-	l.state = lex
-
+func (l *Lexer) Lex() {
+	l.eat()
 	for l.state != nil {
 		l.state = l.state(l)
 	}
-
-	if l.scan.ErrorCount > 0 {
-		return &LexError{
-			l.scan.ErrorCount,
-			l.scan.Filename,
-		}
-	}
-
-	return nil
 }
 
-func (l *Lexer) emit(kind token.TokenKind, value string) {
+func (l *Lexer) emit(kind token.TokenKind) {
 	l.tokens <- token.Token{
 		kind,
-		l.pos.Line,
-		l.pos.Column,
-		l.pos.Offset,
-		value,
+		l.buf.String(),
+		l.start,
+		l.current,
+		l.file,
+	}
+	l.buf.Truncate(0)
+	l.start = l.current
+}
+
+func (l *Lexer) emitIdent() {
+	s := l.buf.String()
+	if len(s) == 1 {
+		// Shortcut because no keyword is one character. It must be identifier
+		l.emit(token.IDENT)
+		return
+	}
+
+	switch s {
+	case "true", "false":
+		l.emit(token.BOOL)
+	case "if":
+		l.emit(token.IF)
+	case "then":
+		l.emit(token.THEN)
+	case "else":
+		l.emit(token.ELSE)
+	case "let":
+		l.emit(token.LET)
+	case "in":
+		l.emit(token.IN)
+	case "rec":
+		l.emit(token.REC)
+	case "Array":
+		l.emit(token.ARRAY)
+	case "create":
+		l.emit(token.CREATE)
+	default:
+		l.emit(token.IDENT)
 	}
 }
 
-// Assume '(' of '(*' was consumed
+func (l *Lexer) emitIllegal(reason string) {
+	l.tokens <- token.Token{
+		token.ILLEGAL,
+		reason,
+		l.current,
+		l.current,
+		l.file,
+	}
+	l.buf.Truncate(0)
+	l.start = l.current
+}
+
+func (l *Lexer) emitExpected(s string, a rune) {
+	l.tokens <- token.Token{
+		token.ILLEGAL,
+		fmt.Sprintf("Expected %s but got '%c'", s, a),
+		l.current,
+		l.current,
+		l.file,
+	}
+	l.buf.Truncate(0)
+	l.start = l.current
+}
+
+func (l *Lexer) emitUnexpectedEOF(expected string) {
+	if len(expected) == 1 {
+		l.emitIllegal(fmt.Sprintf("Expected '%s' but got EOF", expected))
+	} else {
+		l.emitIllegal(fmt.Sprintf("Expected one of '%s' but got EOF", expected))
+	}
+}
+
+func (l *Lexer) eat() {
+	r, size, err := l.src.ReadRune()
+	if err == io.EOF {
+		l.top = 0
+		l.eof = true
+		return
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	l.current.Offset += size
+	if r == '\n' {
+		l.current.Line += 1
+		l.current.Column = 1
+	} else {
+		l.current.Column += 1
+	}
+	l.buf.WriteRune(r)
+
+	l.top = r
+	l.eof = false
+}
+
 func lexComment(l *Lexer) stateFn {
-	var b bytes.Buffer
-
-	// Eat '*' of '(*'
-	l.scan.Next()
-
-	// Temporarily be aware of whitespaces in comments
-	l.scan.Whitespace = uint64(0)
-
 	for {
-		r := l.scan.Scan()
-		fmt.Printf("'%c'\n", r)
-		switch l.scan.Scan() {
-		case '*':
-			if l.scan.Peek() == ')' {
-				l.emit(token.COMMENT, b.String())
-				l.scan.Next()
-				l.scan.Whitespace = scanner.GoWhitespace
+		if l.eof {
+			l.emitUnexpectedEOF("*")
+			return nil
+		}
+		if l.top == '*' {
+			l.eat()
+			if l.eof {
+				l.emitUnexpectedEOF(")")
+				return nil
+			}
+			if l.top == ')' {
+				l.eat()
+				l.emit(token.COMMENT)
 				return lex
 			}
-		case scanner.EOF:
-			l.scan.Error(&l.scan, "Unclosed comment")
-			l.emit(token.ILLEGAL, "")
-			l.scan.Whitespace = scanner.GoWhitespace
-			return lex
-		case scanner.Int, scanner.Float, scanner.Ident:
-			b.WriteString(l.scan.TokenText())
-		default:
-			b.WriteRune(r)
 		}
+		l.eat()
 	}
 }
 
-// TODO: Emit identifier or keywords
-// func (l* Lexer) emitIdentifier(val string) {
+func lexLeftParen(l *Lexer) stateFn {
+	l.eat()
+	if l.top == '*' {
+		l.eat()
+		return lexComment
+	} else {
+		l.emit(token.LPAREN)
+		return lex
+	}
+}
 
-func lex(l *Lexer) stateFn {
-Loop:
-	for {
-		r := l.scan.Scan()
-		switch r {
-		case '(':
-			if l.scan.Peek() == '*' {
-				return lexComment
-			} else {
-				l.emit(token.LPAREN, token.TokenStrings[token.LPAREN])
-			}
-		case ')':
-			l.emit(token.RPAREN, token.TokenStrings[token.RPAREN])
-		case scanner.Int, scanner.Float:
-			l.emit(token.NUMBER, l.scan.TokenText())
-		case scanner.Ident:
-			l.emit(token.IDENT, l.scan.TokenText())
-		case scanner.EOF:
-			break Loop
-		default:
-			l.scan.Error(&l.scan, fmt.Sprintf("Unexpected character '%c'", r))
-			l.emit(token.ILLEGAL, "")
-			break Loop
+func lexAdditiveOp(l *Lexer) stateFn {
+	dot, op := token.PLUS_DOT, token.PLUS
+	if l.top == '-' {
+		dot, op = token.MINUS_DOT, token.MINUS
+	}
+	l.eat()
+
+	if l.top == '.' {
+		l.eat()
+		l.emit(dot)
+	} else {
+		l.emit(op)
+	}
+	return lex
+}
+
+func lexMultOp(l *Lexer) stateFn {
+	op := token.STAR_DOT
+	if l.top == '/' {
+		op = token.SLASH_DOT
+	}
+	l.eat()
+
+	if l.top != '.' {
+		l.emitExpected("'.'", l.top)
+		return nil
+	}
+
+	l.eat()
+	l.emit(op)
+	return lex
+}
+
+func lexLess(l *Lexer) stateFn {
+	l.eat()
+	switch l.top {
+	case '>':
+		l.eat()
+		l.emit(token.LESS_GREATER)
+	case '=':
+		l.eat()
+		l.emit(token.LESS_EQUAL)
+	case '-':
+		l.eat()
+		l.emit(token.LESS_MINUS)
+	default:
+		l.emit(token.LESS)
+	}
+	return lex
+}
+
+func lexGreater(l *Lexer) stateFn {
+	l.eat()
+	switch l.top {
+	case '=':
+		l.eat()
+		l.emit(token.GREATER_EQUAL)
+	default:
+		l.emit(token.GREATER)
+	}
+	return lex
+}
+
+// e.g. 123.45e10
+func lexNumber(l *Lexer) stateFn {
+	tok := token.INT
+
+	// Eat first digit. It's known as digit in lex()
+	l.eat()
+	for unicode.IsDigit(l.top) {
+		l.eat()
+	}
+
+	// Note: Allow 1. as 1.0
+	if l.top == '.' {
+		tok = token.FLOAT
+		l.eat()
+		for unicode.IsDigit(l.top) {
+			l.eat()
 		}
 	}
-	l.emit(token.EOF, "")
-	return nil
+
+	if l.top == 'e' {
+		tok = token.FLOAT
+		l.eat()
+		if l.top == '+' || l.top == '-' {
+			l.eat()
+		}
+		if !unicode.IsDigit(l.top) {
+			l.emitExpected("number", l.top)
+			return nil
+		}
+		for unicode.IsDigit(l.top) {
+			l.eat()
+		}
+	}
+
+	l.emit(tok)
+	return lex
+}
+
+func isLetter(r rune) bool {
+	return 'a' <= r && r <= 'z' ||
+		'A' <= r && r <= 'Z' ||
+		r == '_' ||
+		r >= utf8.RuneSelf && unicode.IsLetter(r)
+}
+
+func lexIdent(l *Lexer) stateFn {
+	if !isLetter(l.top) {
+		l.emitExpected("letter", l.top)
+		return nil
+	}
+	l.eat()
+
+	for isLetter(l.top) || unicode.IsDigit(l.top) {
+		l.eat()
+	}
+
+	l.emitIdent()
+	return lex
+}
+
+func lex(l *Lexer) stateFn {
+	for {
+		if l.eof {
+			l.emit(token.EOF)
+			return nil
+		}
+		switch l.top {
+		case '(':
+			return lexLeftParen
+		case ')':
+			l.eat()
+			l.emit(token.RPAREN)
+		case '+':
+			return lexAdditiveOp
+		case '-':
+			return lexAdditiveOp
+		case '*':
+			return lexMultOp
+		case '/':
+			return lexMultOp
+		case '=':
+			l.eat()
+			l.emit(token.EQUAL)
+		case '<':
+			return lexLess
+		case '>':
+			return lexGreater
+		case ',':
+			l.eat()
+			l.emit(token.COMMA)
+		case '.':
+			l.eat()
+			l.emit(token.DOT)
+		case ';':
+			l.eat()
+			l.emit(token.SEMICOLON)
+		default:
+			switch {
+			case unicode.IsSpace(l.top):
+				l.eat()
+			case unicode.IsDigit(l.top):
+				return lexNumber
+			default:
+				return lexIdent
+			}
+		}
+	}
 }
--- token/token.go
+++ token/token.go
@@ -4,37 +4,101 @@ import (
 	"fmt"
 )
 
-type Pos int
+type Position struct {
+	Offset int
+	Line   int
+	Column int
+}
+
 type TokenKind int
 
 const (
 	ILLEGAL TokenKind = iota
 	EOF
 	COMMENT
-	NUMBER
 	LPAREN
 	RPAREN
 	IDENT
+	BOOL
+	NOT
+	INT
+	FLOAT
+	MINUS
+	PLUS
+	MINUS_DOT
+	PLUS_DOT
+	STAR_DOT
+	SLASH_DOT
+	EQUAL
+	LESS_GREATER
+	LESS_EQUAL
+	LESS
+	GREATER
+	GREATER_EQUAL
+	IF
+	THEN
+	ELSE
+	LET
+	IN
+	REC
+	COMMA
+	ARRAY
+	CREATE
+	DOT
+	LESS_MINUS
+	SEMICOLON
 )
 
 var TokenStrings = [...]string{
-	ILLEGAL: "ILLEGAL",
-	EOF:     "EOF",
-	COMMENT: "COMMENT",
-	NUMBER:  "NUMBER",
-	LPAREN:  "(",
-	RPAREN:  ")",
-	IDENT:   "IDENT",
+	ILLEGAL:       "ILLEGAL",
+	EOF:           "EOF",
+	COMMENT:       "COMMENT",
+	LPAREN:        "(",
+	RPAREN:        ")",
+	IDENT:         "IDENT",
+	BOOL:          "BOOL",
+	NOT:           "not",
+	INT:           "INT",
+	FLOAT:         "FLOAT",
+	MINUS:         "-",
+	PLUS:          "+",
+	MINUS_DOT:     "-.",
+	PLUS_DOT:      "+.",
+	STAR_DOT:      "*.",
+	SLASH_DOT:     "/.",
+	EQUAL:         "=",
+	LESS_GREATER:  "<>",
+	LESS_EQUAL:    "<=",
+	LESS:          "<",
+	GREATER:       ">",
+	GREATER_EQUAL: ">=",
+	IF:            "if",
+	THEN:          "then",
+	ELSE:          "else",
+	LET:           "let",
+	IN:            "in",
+	REC:           "rec",
+	COMMA:         ",",
+	ARRAY:         "Array",
+	CREATE:        "create",
+	DOT:           ".",
+	LESS_MINUS:    "<-",
+	SEMICOLON:     ";",
 }
 
 type Token struct {
-	Kind   TokenKind
-	Line   int
-	Column int
-	Offset int
-	Value  string
+	Kind  TokenKind
+	Value string
+	Start Position
+	End   Position
+	File  string
 }
 
 func (tok *Token) String() string {
-	return fmt.Sprintf("<%s:%d:%d:%d:%s>", TokenStrings[tok.Kind], tok.Line, tok.Column, tok.Offset, tok.Value)
+	return fmt.Sprintf(
+		"<%s:%s>(%d:%d:%d-%d:%d:%d)",
+		TokenStrings[tok.Kind],
+		tok.Value,
+		tok.Start.Line, tok.Start.Column, tok.Start.Offset,
+		tok.End.Line, tok.End.Column, tok.End.Offset)
 }
