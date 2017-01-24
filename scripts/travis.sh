0f9bbae89f3c0bf3ea09bd91818b82c641ca6291
rhysd <lin90162@yahoo.co.jp>
2017-01-24 17:56:35 +0900

    prepare for Travis CI

---
+++ scripts/travis.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+set -e
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    brew update
+    brew upgrade go
+    go get -t -d -v ./...
+    go test -v ./...
+else
+    go get golang.org/x/tools/cmd/cover
+    go get github.com/haya14busa/goverage
+    go get -t -d -v ./...
+    go test -v ./...
+    if [[ $? == 0 ]]; then
+        goverage -coverprofile cover.out ./...
+        go tool cover -func cover.out
+    fi
+fi
+
