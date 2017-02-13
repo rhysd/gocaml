4daa6a3b1c689cf7f278c89d90e4c249c2d82db5
rhysd <lin90162@yahoo.co.jp>
2017-02-13 12:00:24 +0900

    fix preparing to run tests on Travis CI

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -5,14 +5,12 @@ set -e
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew update
     brew upgrade go
-    go get -t -d -v ./...
     make build
     go test -v ./...
 else
     go get golang.org/x/tools/cmd/cover
     go get github.com/haya14busa/goverage
     go get github.com/mattn/goveralls
-    go get -t -d -v ./...
     make build
     go test -v ./...
     make cover.out
