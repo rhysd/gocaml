e8565603329dc49daed2bff1506a147e144fda3d
rhysd <lin90162@yahoo.co.jp>
2017-03-29 20:19:11 +0900

    fix test case for syntax check (match expression)

--- testdata/syntax/match.ml
+++ testdata/syntax/match.ml
@@ -2,7 +2,7 @@ match Some 42 with
   | Some i -> "ok"
   | None   -> "not ok";
 let s = match None with None -> "none" | Some i -> "some" in
-match Some Some s with
+match Some (Some s) with
     None -> Some 10
   | Some o -> Some 99;
 ()
