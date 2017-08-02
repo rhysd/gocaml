f560e8305caa0c780863c75855451e99f848ec47
rhysd <lin90162@yahoo.co.jp>
2017-08-03 06:30:23 +0900

    add fizzbuzz example

---
+++ examples/fizzbuzz2.ml
@@ -0,0 +1,14 @@
+let rec fizzbuzz max =
+    let rec fb n =
+        if n % 15 = 0 then println_str "fizzbuzz" else
+        if n % 3 = 0  then println_str "fizz" else
+        if n % 5 = 0  then println_str "buzz" else
+        println_int n
+    in
+    let rec go n =
+        if n = max then () else
+        (fb n; go (n+1))
+    in
+    go 1
+in
+fizzbuzz 100
