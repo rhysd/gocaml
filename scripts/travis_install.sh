2b01445b17eb3cf5d2877f6ccdf7859ae8b5443b
rhysd <lin90162@yahoo.co.jp>
2018-03-19 16:00:37 +0900

    do not set $CC and $CXX on macOS worker

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -13,6 +13,8 @@ else
     go get github.com/haya14busa/goverage
     go get github.com/mattn/goveralls
     export LLVM_CONFIG="llvm-config-6.0"
+    export CC=gcc-7
+    export CXX=g++-7
 fi
 
 make build
